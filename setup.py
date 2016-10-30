b7ccb9b8f93c7eb5fc78bdd9b23612481c489b17
Yi FU <yvesfu@gmail.com>
2016-10-30 23:22:41 +0100

    Package structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pypin',
+      version='0.1',
+      description='Python client for Pinterest API',
+      author='Yi FU',
+      author_email='yvesfu@gmail.com',
+      license='MIT',
+      packages=['pypin'],
+      zip_safe=False)
